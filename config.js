cd54ce2cfeaef519551f8d47f0e584e9fdb09a3c
Chris McCaulley <talkquazi@gmail.com>
2015-02-20 17:06:45 -0500

    - Initial Commit

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	rfcname: 'Harpoon File Send Server',
+	localip: '127.0.0.1',
+	hosts: ['localhost', '127.0.0.1'],									// Allowed Hosts For Clustering (This should always have localhost and 127.0.0.1)
+	peers: ['127.0.0.1'],											// Allowed Peers For cluster Peering
+	filesDirectory: __dirname + "files/",									// Absolute path with trailing slash or relative path with trailing slash
+	port: 8080,												// Server Port
+	maxLag: 5000,                                                              				// Maximum Wait Time In Milliseconds
+	serverSecret: "ASaltyPhras3",                                        				// Used For Session Caching
+	logging: true,                                                             				// Enable or Disable Logging
+	basekbps: 1000,                                                            				// Minimum rate to attempt to send files at in kb
+	burstkbps: 500,                                                           				// The burst rate for sending files
+	exts: ['.zip', '.rar', '.7zip', '.png', '.jpg', '.jpeg', '.gif', '.swf', '.flv', '.txt', 'status'], 	// File Types To Protect From Leeching
+	picture: options.picture || __dirname + "/files/default.png"    
+}
---
+++ server.js
@@ -0,0 +1,27 @@
+﻿/*
+ * Harpoon - A Static File Server
+ *
+ * Features:
+ *  - Session Clustering
+ *  - Server Health Data
+ *  - Server Clustering
+ *  - Worker Threading
+ *  - Lag Overload Prevention
+ *  - Session Caching
+ *  - Session Security
+ *  - Bot Detection
+ *  - Spider Detection
+ *  - Throttle Resources
+ *  - Resource Caching
+ *  - Anti-Leeching
+ *  - Anti-Query Scanning
+ *  - Prevents SQL Injections
+ *  - Throttles Requests
+ *  - QoS Uptime
+ *  - Request Logging
+ *
+ */
+
+module.exports = function(options) {
+	require('./lib/harpoon.js')(options);
+}
